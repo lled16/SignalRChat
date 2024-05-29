2ae5712ef8a95bb1325dd86d23f9e3dc031d1a43
joao.simao <vitoralves1604@gmail.com>
2024-05-29 17:13:11 -0300

    Feat: Add sending files, refatored design

--- SignalRChat/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ SignalRChat/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bd2f1df170887cb85cdb96da2b28b257864800")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
